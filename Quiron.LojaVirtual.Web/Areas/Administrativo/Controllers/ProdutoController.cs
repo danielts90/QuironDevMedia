e58d9f468d8581bd344874e5933ea667acf5e05e
danielts90 <danieltadeu2.dt@gmail.com>
2017-08-15 19:33:35 -0300

    Aula 29 - CRUD de Produtos

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutosRepositorio _repositorio = new ProdutosRepositorio();
+        
+        public ActionResult Index()
+        {
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
+        }
+    }
+}
