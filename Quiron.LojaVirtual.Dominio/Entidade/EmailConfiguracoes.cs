967bae257ea358950793f317b121d73f5031e371
danielts90 <danieltadeu2.dt@gmail.com>
2017-08-15 15:53:18 -0300

    Aula 26 - Classes de email

---
+++ Quiron.LojaVirtual.Dominio/Entidade/EmailConfiguracoes.cs
@@ -0,0 +1,14 @@
+﻿namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+        public string ServidorSmtp = "meusmtp";
+        public int ServidorPorta = 587;
+        public string Usuario = "quiron";
+        public bool EscreverArquivo = false;
+        public string PastaArquivo = @"C:\envioEmail";
+        public string De = "quiron@quiron.com.br";
+        public string Para = "pedido@pedido.com.br";
+    }
+}
