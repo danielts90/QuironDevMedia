38f200d8513bf82807e15c867eef37bfa31960a8
danielts90 <danieltadeu2.dt@gmail.com>
2017-08-20 00:28:33 -0300

    Aula 38 - Classe Administradores e controle de acesso

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Administradores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Administradores
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage ="Digite o login")]
+        [Display(Name ="Login")]
+        public string Login { get; set; }
+        [Required(ErrorMessage ="Digite a senha")]
+        [Display(Name ="Senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        public DateTime UltimoAcesso { get; set; }
+    }
+}
