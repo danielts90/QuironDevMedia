26d90dad6c1bf6408b1886a0a76be8c191fbe4ed
danielts90 <danieltadeu2.dt@gmail.com>
2017-08-25 00:00:14 -0300

    Aulas 46 e 47 - ModelBindng e CustomModelBinding

---
+++ Quiron.LojaVirtual.Web/Controllers/ModelBindingController.cs
@@ -0,0 +1,25 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        // GET: ModelBindign
+        public ActionResult Index()
+        {
+            return View(new Produto());
+        }
+
+        [HttpPost]
+        public ActionResult Editar()
+        {
+            var produto = new Produto();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Infraestrutura/CarrinhoModelBinder.cs
@@ -0,0 +1,34 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Infraestrutura
+{
+    public class CarrinhoModelBinder : System.Web.Mvc.IModelBinder
+    {
+        private const string SessionKey = "Carrinho";
+        public object BindModel(ControllerContext controllerContext, System.Web.Mvc.ModelBindingContext bindingContext)
+        {
+            //Obter Carrinho da sessão
+            Carrinho carrinho = null;
+
+            if(controllerContext.HttpContext.Session != null)
+            {
+                carrinho = (Carrinho)controllerContext.HttpContext.Session[SessionKey];
+            }
+
+            //Criar o carrinho 
+            if(carrinho == null)
+            {
+                carrinho = new Carrinho();
+
+                if(controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[SessionKey] = carrinho;
+                }
+            }
+
+            return carrinho;
+
+        }
+    }
+}
