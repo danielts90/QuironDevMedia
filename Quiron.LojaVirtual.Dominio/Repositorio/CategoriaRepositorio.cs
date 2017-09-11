351c7c28b85c7400854be3cffaddf88569cb0145
danielts90 <danieltadeu2.dt@gmail.com>
2017-09-10 21:57:26 -0300

    Aula 56 - Novo layout, criação do projeto web2

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class CategoriaRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Categoria> ObterCategoria()
+        {
+            return _context.Categoria.OrderBy(c => c.CategoriaDescricao);
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web.V2/Controllers/CategoriaController.cs
@@ -0,0 +1,30 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System.Web.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.V2.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private CategoriaRepositorio _repositorio = new CategoriaRepositorio();
+       
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Server, VaryByParam = "none")]
+        public JsonResult ObterEsportes()
+        {
+            var cat = _repositorio.ObterCategoria();
+            var categorias = from c in cat
+                             select new
+                             {
+                                 c.CategoriaDescricao,
+                                 CategoriaDescricaoSeo = c.CategoriaDescricao.ToSeoUrl(),
+                                 c.CategoriaCodigo
+                             };
+            return Json(categorias, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
