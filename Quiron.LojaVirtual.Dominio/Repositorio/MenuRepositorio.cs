073bcc01f802c4354996e902ecba22bf775cbe88
danielts90 <danieltadeu2.dt@gmail.com>
2017-09-10 22:40:15 -0300

    Aula 58 - Menu de marcas

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/MenuRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using Quiron.LojaVirtual.Dominio.Entidade.Vitrine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class MenuRepositorio
+    {
+        private EfDbContext _context = new EfDbContext();
+        public IEnumerable<Categoria> ObterCategorias()
+        {
+            return _context.Categorias.OrderBy(c => c.CategoriaDescricao);
+        }
+
+        public IEnumerable<MarcaVitrine> ObterMarcas()
+        {
+            return _context.MarcaVitrine.OrderBy(c => c.MarcaDescricao);
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web.V2/Controllers/MenuController.cs
@@ -0,0 +1,44 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Quiron.LojaVirtual.Web.V2.Controllers
+{
+    public class MenuController : Controller
+    {
+        private MenuRepositorio _repositorio = new MenuRepositorio();
+
+
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Server, VaryByParam = "none")]
+        public JsonResult ObterEsportes()
+        {
+            var cat = _repositorio.ObterCategorias();
+            var categorias = from c in cat
+                             select new
+                             {
+                                 c.CategoriaDescricao,
+                                 CategoriaDescricaoSeo = c.CategoriaDescricao.ToSeoUrl(),
+                                 c.CategoriaCodigo
+                             };
+            return Json(categorias, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObterMarcas()
+        {
+            var listaMarcas = _repositorio.ObterMarcas();
+
+            var marcas = from m in listaMarcas
+                         select new
+                         {
+                             m.MarcaDescricao,
+                             MarcaDescricaoSeo = m.MarcaDescricao.ToSeoUrl(),
+                             m.MarcaCodigo
+                         };
+            return Json(marcas, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
