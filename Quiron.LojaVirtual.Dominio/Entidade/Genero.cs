8b284911daeedb721f827d5a4f60c6937363ce7c
danielts90 <danieltadeu2.dt@gmail.com>
2017-09-21 22:15:07 -0300

    Aula 63 - Inclusão das novas entidades no DbContext

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Genero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Genero
+    {
+        public int GeneroId { get; set; }
+        public string GeneroCodigo { get; set; }
+        public string GeneroDescricao { get; set; }
+    }
+}
---
+++ Quiron.LojaVirtual.Dominio/Entidade/Marca.cs
@@ -0,0 +1,9 @@
+﻿namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Marca
+    {
+        public int MarcaId { get; set; }
+        public string MarcaCodigo { get; set; }
+        public string MarcaDescricao { get; set; }
+    }
+}
