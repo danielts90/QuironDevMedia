cb2a7a59653a14ea99a0d69c3e7881bd0aacc9cb
danielts90 <danieltadeu2.dt@gmail.com>
2017-09-26 22:06:16 -0300

    Aula 72 - Menu Casual

---
+++ Quiron.LojaVirtual.Web.V2/Models/ModalidadeSubGrupoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Quiron.LojaVirtual.Dominio.Dto;
+using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Collections.Generic;
+
+namespace Quiron.LojaVirtual.Web.V2.Models
+{
+    public class ModalidadeSubGrupoViewModel
+    {
+        public Modalidade Modalidade { get; set; }
+        public IEnumerable<SubGrupoDto> SubGrupos { get; set; }
+    }
+
+}
